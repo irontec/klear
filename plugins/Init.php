0c93cbe9a235924974d690bc5cbaad6fd141df0c
Alayn Gortazar <zutoin@gmail.com>
2012-01-27 23:16:41 +0000

    Refactorizado Plugin Init
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@38 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- plugins/Init.php
+++ plugins/Init.php
@@ -1,59 +1,91 @@
 <?php
+/**
+ * Plugin encargado de inicializar los recursos necesarios en Klear
+ * @author Jabi Infante
+ *
+ */
+class Klear_Plugin_Init extends Zend_Controller_Plugin_Abstract
+{
+    /**
+     * @var Zend_Controller_Front
+     */
+    protected $_front;
 
-class Klear_Plugin_Init extends Zend_Controller_Plugin_Abstract {
-
-
-    public function preDispatch(Zend_Controller_Request_Abstract $request)
+    public function __construct()
     {
-
-        $module = $request->getModuleName();
-        if (!preg_match("/^klear/",$module)) return;
-
-        $front = Zend_Controller_Front::getInstance();
-
-        /**
-         * Indicamos ruta del layout de klear
-         */
-        Zend_Layout::startMvc();
-        $layout = Zend_Layout::getMvcInstance();
-        $layout->setLayoutPath( $front->getModuleDirectory() . '/layouts/scripts');
-	    $layout->setLayout('layout');
-
-
-
-		/*
-		 * Carga configuración principal de klear
-		 */
-		$config = new Zend_Config_Yaml(
-				APPLICATION_PATH . '/configs/klear/klear.yaml',
-				APPLICATION_ENV
-		);
-
-
-		$klearConfig = new Klear_Model_MainConfig();
-		$klearConfig->setConfig($config);
-
-		/*
-		 * Recupearmos bootstrap para usar su contenedor para guardar
-		 */
-
-		$bootstrap = $front = Zend_Controller_Front::getInstance()->getParam("bootstrap");
-
-		$bootstrap
-		        ->getResource('modules')
-		        ->offsetGet('klear')
-		        ->setOptions(array(
-							"siteConfig"=>$klearConfig->getSiteConfig(),
-							"menu"=>$klearConfig->getMenu())
-		);
-
-
+        $this->_front = Zend_Controller_Front::getInstance();
     }
 
+    /**
+     * Este método que se ejecuta una vez se ha matcheado la ruta adecuada
+     * (non-PHPdoc)
+     * @see Zend_Controller_Plugin_Abstract::routeShutdown()
+     */
     public function routeShutdown(Zend_Controller_Request_Abstract $request)
     {
-        $front = Zend_Controller_Front::getInstance();
-        $error = $front->getPlugin('Zend_Controller_Plugin_ErrorHandler');
+        if (!preg_match("/^klear/", $request->getModuleName())) {
+            return;
+        }
+
+        $this->_initErrorHandler();
+        $this->_initConfig();
+        $this->_initLayout();
+    }
+
+    /**
+     * Inicializa el Layout de Klear dirigiéndolo a la ruta adecuada
+     */
+    protected function _initLayout()
+    {
+        /*
+         * Indicamos ruta del layout de klear
+        */
+        Zend_Layout::startMvc();
+        $layout = Zend_Layout::getMvcInstance();
+        $layout->setLayoutPath($this->_front->getModuleDirectory() . '/layouts/scripts');
+        $layout->setLayout('layout');
+    }
+
+    /**
+     * Inicializa la configuración principal de Klear
+     * y la almacena en el recurso de módulos del bootstrap
+     */
+    protected function _initConfig()
+    {
+        /*
+         * Cargamos la configuración
+         */
+        $config = new Zend_Config_Yaml(
+            APPLICATION_PATH . '/configs/klear/klear.yaml',
+            APPLICATION_ENV
+        );
+
+        $klearConfig = new Klear_Model_MainConfig();
+        $klearConfig->setConfig($config);
+
+        /*
+         * Guardamos la configuración en el recurso de modulos
+         */
+        $bootstrap = $this->_front->getParam("bootstrap");
+        $bootstrap
+        ->getResource('modules')
+        ->offsetGet('klear')
+        ->setOptions(
+            array(
+                "siteConfig"=>$klearConfig->getSiteConfig(),
+                "menu"=>$klearConfig->getMenu()
+            )
+        );
+    }
+
+    protected function _initErrorHandler()
+    {
+        if ($this->_front->hasPlugin('Zend_Controller_Plugin_ErrorHandler')) {
+            $error = $this->_front->getPlugin('Zend_Controller_Plugin_ErrorHandler');
+        } else {
+            $error = new Zend_Controller_Plugin_ErrorHandler();
+            $this->_front->registerPlugin($error);
+        }
         $error
         ->setErrorHandlerModule('klear')
         ->setErrorHandlerController('error')
