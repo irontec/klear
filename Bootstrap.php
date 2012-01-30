c4754c06ab44337216ae13c72aee08450e3a5eea
Alayn Gortazar <zutoin@gmail.com>
2012-01-30 00:53:40 +0000

    Algo de refactorización, limpieza y algún bugfix
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@39 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,88 +2,86 @@
 
 class Klear_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-
-
 	/**
-	 * Registramos plugin que se encarga de discriminar cargas desde fuera del módulo
+	 * Registramos el plugin que se encarga de inicializar Klear
+	 * solo para los módulos de Klear
 	 */
 	protected function _initKlear()
 	{
-
 		$front = Zend_Controller_Front::getInstance();
-
         $front->registerPlugin(new Klear_Plugin_Init());
 	}
 
-	protected function _initJsRoutes()
+	protected function _initAssetRoutes()
 	{
-
 		$frontController = Zend_Controller_Front::getInstance();
 		$router = $frontController->getRouter();
 
 		$router->addRoute(
 			'klearScripts',
 			$route = new Zend_Controller_Router_Route_Regex(
-						'(klear[^/]*)/js/(.*)$',
-						array(
-							'controller' => 'assets',
-							'action' => 'js',
-							'module' => 'klear'
-						),
-						array(
-								2 => 'file',
-								1 => 'moduleName'
-						)
-				)
-		);
-
-
-		$router->addRoute(
-				'klearCss',
-				$route = new Zend_Controller_Router_Route_Regex(
-						'(klear[^/]*)/css/(.*)$',
-						array(
-								'controller' => 'assets',
-								'action' => 'css',
-								'module' => 'klear'
-						),
-						array(
-								2 => 'file',
-								1 => 'moduleName'
-						)
-				)
+    			'(klear[^/]*)/js/(.*)$',
+    			array(
+    				'controller' => 'assets',
+    				'action' => 'js',
+    				'module' => 'klear'
+    			),
+    			array(
+					2 => 'file',
+					1 => 'moduleName'
+    			)
+			)
 		);
 
 		$router->addRoute(
-				'klearImagesinCss',
-				$route = new Zend_Controller_Router_Route_Regex(
-						'(klear[^/]*)/css/(.*\.png)$',
-						array(
-								'controller' => 'assets',
-								'action' => 'imageCss',
-								'module' => 'klear'
-						),
-						array(
-								2 => 'file',
-								1 => 'moduleName'
-						)
+			'klearCss',
+			$route = new Zend_Controller_Router_Route_Regex(
+				'(klear[^/]*)/css/(.*)$',
+				array(
+					'controller' => 'assets',
+					'action' => 'css',
+					'module' => 'klear'
+				),
+				array(
+					2 => 'file',
+					1 => 'moduleName'
 				)
+			)
+		);
+
+		/*
+		 * TODO: Preparar la expresión regular para que soporte más tipos de imágenes
+		 */
+		$router->addRoute(
+	        'klearCssImages',
+	        $route = new Zend_Controller_Router_Route_Regex(
+                '(klear[^/]*)/css/(.*\.png)$',
+                array(
+                    'controller' => 'assets',
+                    'action' => 'css-image',
+                    'module' => 'klear'
+                ),
+                array(
+                    2 => 'file',
+                    1 => 'moduleName'
+                )
+	        )
 		);
 
 		$router->addRoute(
-				'klearImages',
-				$route = new Zend_Controller_Router_Route_Regex(
-						'(klear[^/]*)/images/(.*)$',
-						array(
-								'controller' => 'assets',
-								'action' => 'image',
-								'module' => 'klear'
-						),
-						array(
-								2 => 'file',
-								1 => 'moduleName'
-						)
+			'klearImages',
+			$route = new Zend_Controller_Router_Route_Regex(
+				'(klear[^/]*)/images/(.*)$',
+				array(
+					'controller' => 'assets',
+					'action' => 'image',
+					'module' => 'klear'
+				),
+				array(
+					2 => 'file',
+					1 => 'moduleName'
 				)
+			)
 		);
 
 	}
--- controllers/AssetsController.php
+++ controllers/AssetsController.php
@@ -2,174 +2,176 @@
 
 class Klear_AssetsController extends Zend_Controller_Action
 {
-
     public function init()
     {
         $this->_helper->layout->disableLayout();
         $this->_helper->getHelper('viewRenderer')->setNoRender();
     }
 
-    
-    protected function _buildPath($base) {
-    	$front = $this->getFrontController();
-    	
-    	$moduleDirectory = $front->getModuleDirectory($this->getRequest()->getParam('moduleName'));
+    protected function _buildPath($base)
+    {
+        $front = $this->getFrontController();
+        $moduleDirectory = $front->getModuleDirectory($this->getRequest()->getParam('moduleName'));
         return $moduleDirectory . $base . $this->getRequest()->getParam("file");
     }
-    
-    public function jsAction() {
-    	
-    	$jsFile = $this->_buildPath('/assets/js/');
-    	$this->_compress($jsFile,"js");
+
+    public function jsAction()
+    {
+        $jsFile = $this->_buildPath('/assets/js/');
+        $this->_compress($jsFile, "js");
     }
-    
-    
-    
-    public function cssAction() {
-    	
-    	$cssFile = $this->_buildPath('/assets/css/');
-    	$this->_compress($cssFile,"css");
+
+    public function cssAction()
+    {
+        $cssFile = $this->_buildPath('/assets/css/');
+        $this->_compress($cssFile, "css");
     }
-    
-    
-	public function imagecssAction() {
+
+    public function cssImageAction()
+    {
         $imgFile = $this->_buildPath('/assets/css/');
-        return $this->_imgAction($imgFile);
-                
+        return $this->_sendImage($imgFile);
     }
-    
-    public function imageAction() {
-    	$imgFile = $this->_buildPath('/assets/images/');
-    	return $this->_imgAction($imgFile);
-    
+
+    public function imageAction()
+    {
+        $imgFile = $this->_buildPath('/assets/images/');
+        return $this->_sendImage($imgFile);
     }
-    
-    protected function _imgAction($file) {
-        
-        // To-DO: Cachear propiedades de cada imagen?
-        $image = new imagick($file);
+
+    /**
+     * TODO: Esto es carne de Action Helper
+     * @param string $file
+     */
+    protected function _sendImage($file)
+    {
+        // TODO: Cachear propiedades de cada imagen?
+        $image = new Imagick($file);
         $hash = $image->getImageSignature();
-        //TO-DO: Fix 
-        $format = 'image/' . strtolower($image->getImageFormat()); 
-        
+
+        // TODO: Fix
+        /* FIXME: Fix what?? :S */
+        $format = 'image/' . strtolower($image->getImageFormat());
+
         $response = $this->getResponse();
         $request = $this->getRequest();
-        
-    	if (($request->getHeader('If-None-Match')) &&
-    			($request->getHeader('If-None-Match') == $hash)
-    		)
-    		{
-    			$response->setHttpResponseCode(304);
-    			return;
-    	}
-        
-    	
-    	if ("development" !== APPLICATION_ENV) {
-    		$response->setHeader('ETag',$hash);
-    	}
-    	
-		$response->setHeader('Cache-control', 'maxage=' . 60*60*24*30, true);
-        $response->setHeader('Content-type',$format);	
-	   	$response->setHeader('Content-length',filesize($file));
-        readFile($file);	
-        
+
+        if (($request->getHeader('If-None-Match'))
+           && ($request->getHeader('If-None-Match') == $hash)) {
+            $response->setHttpResponseCode(304);
+                return;
+        }
+
+        if ("production" === APPLICATION_ENV) {
+            $response->setHeader('ETag', $hash);
+        }
+
+        $response->setHeader('Cache-control', 'maxage=' . 60*60*24*30, true);
+        $response->setHeader('Content-type', $format);
+        $response->setHeader('Content-length', filesize($file));
+        readFile($file);
     }
-    
-    public function _compress($file,$type)
+
+    public function _compress($file, $type)
     {
-    	
-    	switch(strtolower($type)) {
-    		case "js":
-    			$className = "Iron_Minify_JsMin";
-    			$method = "min";
-    			$file_content_type = 'application/x-javascript';
-    			break;
-    		case "css":
-    			$className = "Iron_Minify_CssCompressor";
-    			$method = "min";
-    			$file_content_type = 'text/css';
-    			break;
-    		default:
-    			Throw new Zend_Exception("Compresor not properly called");
-    	}
-    	
-    	
-    	$lastModifiedTime = filemtime($file);
-    	
-    	// La "firma" del fichero contendrá la fecha de última modificación
-    	// $hashTag => Se aplica este tag a la cache para eliminar de la cache los ficheros viejos
-    	
-    	$hashTag = str_replace(array("/",".","-"),"_",basename($file));
-    	$hash = $hashTag . '__' . date('d_m_Y_H_i_s',$lastModifiedTime);
-    	
-    	$response = $this->getResponse();
-    	$request = $this->getRequest();
-    	
-    	if (($request->getHeader('IF-MODIFIED-SINCE')) &&
-    			(strtotime($request->getHeader('IF-MODIFIED-SINCE')) == $lastModifiedTime)
-    		)
-    		{
-    		
-    			$response->setHttpResponseCode(304);
-    			return;
-    	}
-    	
-    	$this->getFrontController()->setParam('disableOutputBuffering', true);
-    	
-    	$frontendOptions = array(
-    			'lifetime' => null, // Forever! - filemtype is implicit in cache signature!
-    			'memorize_headers' => array('content-type', 'content-length','pragma','cache-control','last-modified'),
-    	        'default_options' => array(
-    	            'tags'=>array($hashTag)
-    	        )
-    	);
-    	
-    	$backendOptions = array(
-    			'cache_dir' => APPLICATION_PATH . '/cache/'
-    			
-    	);
-    	
-    	$cache = Zend_Cache::factory('Page',
-    			'File',
-    			$frontendOptions,
-    			$backendOptions);
-    	
-    	
-		if (!$cache->start($hash)) {
-			
-    		if ("development" === APPLICATION_ENV) {
-    			$data = file_get_contents($file);
-    		} else {
-    			$min = new $className(file_get_contents($file));
-    			$data = $min->{$method}();
-    		}
-    		
-    	    
-    		$response->setHeader('Pragma', 'public', true);
-		    $response->setHeader('Cache-control', 'maxage=' . 60*60*24*30, true);
-		
-		    if ("development" !== APPLICATION_ENV) {
-		    	$response->setHeader('Last-Modified', gmdate('D, d M Y H:i:s', $lastModifiedTime).' GMT', true);
-		    }
-		    
-		    $response->setHeader('Content-type',$file_content_type);	
-	    	$response->setHeader('Content-length',strlen($data));
-    	    $response->sendHeaders();
-    		echo $data;
-    		
-    		// Antes de salvar, elimino todos los "tags" de este fichero
-    	    $cache->clean(
+        $lastModifiedTime = filemtime($file);
+
+        // La "firma" del fichero contendrá la fecha de última modificación
+        // $hashTag => Se aplica este tag a la cache para eliminar de la cache los ficheros viejos
+
+        $hashTag = str_replace(array("/",".","-"), "_", basename($file));
+        $hash = $hashTag . '__' . date('d_m_Y_H_i_s', $lastModifiedTime);
+
+        $response = $this->getResponse();
+        $request = $this->getRequest();
+
+        if (($request->getHeader('IF-MODIFIED-SINCE'))
+           && (strtotime($request->getHeader('IF-MODIFIED-SINCE')) == $lastModifiedTime)) {
+            $response->setHttpResponseCode(304);
+            return;
+        }
+
+        $this->getFrontController()->setParam('disableOutputBuffering', true);
+
+        $frontendOptions = array(
+            'lifetime' => null, // Forever! - mimetype is implicit in cache signature!
+            'memorize_headers' => array(
+                'content-type',
+                'content-length',
+                'pragma',
+                'cache-control',
+                'last-modified'
+            ),
+            'default_options' => array(
+                'tags'=>array($hashTag)
+            )
+        );
+
+        $backendOptions = array(
+            'cache_dir' => APPLICATION_PATH . '/cache/'
+        );
+
+        $cache = Zend_Cache::factory(
+            'Page',
+            'File',
+            $frontendOptions,
+            $backendOptions
+        );
+
+        if (!$cache->start($hash)) {
+            $data = $this->_getContents($file, $type);
+
+            $response->setHeader('Pragma', 'public', true);
+            $response->setHeader('Cache-control', 'maxage=' . 60*60*24*30, true);
+
+            if ("production" === APPLICATION_ENV) {
+                $response->setHeader(
+                    'Last-Modified',
+                    gmdate('D, d M Y H:i:s', $lastModifiedTime) . ' GMT',
+                    true
+                );
+            }
+
+            switch(strtolower($type)) {
+                case "js":
+                    $fileContentType = 'application/x-javascript';
+                    break;
+                case "css":
+                    $fileContentType = 'text/css';
+                    break;
+            }
+
+            $response->setHeader('Content-type', $fileContentType);
+            $response->setHeader('Content-length', strlen($data));
+            $response->sendHeaders();
+            echo $data;
+
+            // Antes de salvar, elimino todos los "tags" de este fichero
+            $cache->clean(
                 Zend_Cache::CLEANING_MODE_MATCHING_TAG,
                 array($hashTag)
             );
-    		$cache->save($data);
-    	}
-    	
-    	
-    	
+            $cache->save($data);
+        }
     }
-  
 
-
+    protected function _getContents($file, $type)
+    {
+        $data = file_get_contents($file);
+        if ("production" === APPLICATION_ENV) {
+            switch(strtolower($type)) {
+                case "js":
+                    $minifier = new Iron_Minify_JsMin($data);
+                    break;
+                case "css":
+                    $minifier = new Iron_Minify_CssCompressor($data);
+                    break;
+                default:
+                    throw new Zend_Exception("Minifier not properly called");
+            }
+            $data = $minifier->min();
+        }
+        return $data;
+    }
 }
 
