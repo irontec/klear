b57ed540dfb13a202a6cf56636b7c09a69e293fb
Lander Ontoria <lander@irontec.com>
2012-04-30 11:07:46 +0000

    ML
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@89 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,15 +5,16 @@ class Klear_IndexController extends Zend_Controller_Action
 
     protected $_auth;
     protected $_loggedIn = false;
-
+    
     public function init()
     {
         /* Initialize action controller here */
     	$this->_helper->ContextSwitch()
     			->addActionContext('dispatch', 'json')
     			->addActionContext('hello', 'json')
+    			->addActionContext('registertranslation', 'json')
     			->initContext('json');
-
+    	
     	$this->_auth = Zend_Auth::getInstance();
 
     }
@@ -38,9 +39,9 @@ class Klear_IndexController extends Zend_Controller_Action
         if ($this->getRequest()->getParam("loginError")) {
             $this->_helper->getHelper('FlashMessenger')->addMessage($this->getRequest()->getParam("loginError"));
         }
-
+        
         $jsonResponse = new Klear_Model_SimpleResponse();
-
+        
         $jsonResponse->setData(
             array(
                 'success'=> true
@@ -49,35 +50,106 @@ class Klear_IndexController extends Zend_Controller_Action
 
         $jsonResponse->attachView($this->view);
     }
+    
+    
+    public function registertranslationAction()
+    {
+        // action body
+    
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+        
+        $namespace = $this->getRequest()->getParam("namespace", false);
+        $str = $this->getRequest()->getParam("str", false);
+        
+        if ($namespace && $str) {
+            list($type, $namespace) = explode("/", $this->getRequest()->getParam("namespace"), 2);
+            list($module, $plugin) = explode(".", $namespace, 2);
+            
+            $modules = Zend_Controller_Front::getInstance()->getControllerDirectory();
+            $mods = array_keys($modules);
+            
+            foreach ($mods as $mod) {
+                if (strtolower($mod) == strtolower($module)) {
+                    $this->getRequest()->setModuleName($mod);
+                    break;
+                }
+            }
+            
+            $bootstrap = $this->getFrontController()->getParam("bootstrap");
+            $klearBootstrap = $bootstrap->getResource('modules')->offsetGet('klear');
+            $siteLanguage = $klearBootstrap->getOption('siteConfig')->getLang();
+           
+            
+            $translationFile = implode(
+                    DIRECTORY_SEPARATOR,
+                    array(
+                            $this->getFrontController()->getModuleDirectory(),
+                            'languages',
+                            'js-translations.php'
+                    )
+            );
+            
+            
+            
+            if (!file_exists($translationFile)) {
+                $contents = array();
+                $fileContents = "<?php\n\n";
+                $fileContents .= "return " . var_export($contents, true) . ";\n";
+                file_put_contents($translationFile, $fileContents);
+            }
+            
+            $jsTranslations = array();
+            $jsTranslations = include($translationFile);
+
+            if (!in_array($str, $jsTranslations)) {
+                $jsTranslations[] = $str;
+                $fileContents = "<?php\n\n";
+                $fileContents .= "return " . var_export($jsTranslations, true) . ";\n";
+                file_put_contents($translationFile, $fileContents);
+                
+            }
+        }
+        $jsonResponse = new Klear_Model_SimpleResponse();
+    
+        $jsonResponse->setData(
+                array(
+                        'success'=> true,
+                )
+        );
+    
+        $jsonResponse->attachView($this->view);
+    }
 
     /**
      * Esta acción redirigirá a la acción del módulo de subsección definido en el fichero de
      *  configuración de sección
      */
     public function dispatchAction()
-    {
-
+    {   
+        
     	$this->_helper->layout->disableLayout();
     	$this->_helper->viewRenderer->setNoRender();
 
-
+    	
     	$file = $this->getRequest()->getParam("file");
 
-    	$filePath = 'klear.yaml://' . $file;
-
-
+    	$file_path = 'klear.yaml://' . $file;
+    	
+    	
     	/*
     	 * Carga configuración de la sección cargada según la request.
     	 */
     	$config = new Zend_Config_Yaml(
-			$filePath,
-			APPLICATION_ENV,
-	        array(
-	                "yamldecoder"=>"yaml_parse"
-	        )
+    			$file_path,
+    			APPLICATION_ENV,
+    	        array(
+    	                "yamldecoder"=>"yaml_parse"
+    	                
+    	        )
     	);
-
-
+    	
+    	
     	// Cargamos el configurador de secciones por defecto
     	$sectionConfig = new Klear_Model_SectionConfig;
     	$sectionConfig->setConfig($config);
@@ -85,7 +157,7 @@ class Klear_IndexController extends Zend_Controller_Action
     		throw new Zend_Controller_Action_Exception("Configuración no válida");
     		return;
     	}
-
+    	
     	if (!$this->_auth->hasIdentity()) {
     	    $this->_forward("hello", "index", "klear");
     	    return;
@@ -104,7 +176,7 @@ class Klear_IndexController extends Zend_Controller_Action
 
     	//Así tendremos disponible la configuración del módulo en el controlador principal.
  		$this->_forward(
- 		    $moduleRouter->getActionName(),
+ 		    $moduleRouter->getActionName(), 
  		    $moduleRouter->getControllerName(),
  		    $moduleRouter->getModuleName(),
  		    array(
