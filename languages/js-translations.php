01bf70dd748478eec386603d67c9103845e4ebdb
Lander Ontoria <lander@irontec.com>
2012-04-30 11:08:29 +0000

    ML
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@92 32e02440-ed3a-45b5-ab12-845b16f45b8b

---
+++ languages/js-translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  0 => 'Module TEST.',
+  1 => 'Dándo por "saco" dos .',
+  2 => 'Dándo por \'saco\' cuatro "xx" .',
+  3 => 'Dándo por \'saco\' uno .',
+  4 => 'Dándo por \'saco\' tres "xx" .',
+);
