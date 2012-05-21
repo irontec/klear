d8cca2ccb40caa84ff4a013ca66a5c690e11363c
Alayn Gortazar <zutoin@gmail.com>
2012-05-21 12:14:26 +0000

    probando el jslint
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@112 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- assets/js/plugins/jquery.getStylesheet.js
+++ assets/js/plugins/jquery.getStylesheet.js
@@ -1,29 +1,31 @@
-;(function($) {
-	$.extend({
+;
+(function($) {
+    $.extend({
 
-	getStylesheet: function(url, id, media) {
-		var stylesheets = $("link[type='text/css']", jQuery("head:first"));
-		
-		var id = id || "rand" + (Math.floor(Math.random()*10000));
-		
-		var el = jQuery('link#'+id, jQuery('head:first'));
-		if (el.length>0) {
-			el.attr('href', url);
-		} else {
-			jQuery('<link />',{
-		        href: url,
-		        media: media || 'screen',
-		        type: 'text/css',
-		        rel: 'stylesheet',
-		        id: id
-		    }).appendTo('head:first');
-		}		
-	},
-	removeStylesheet: function(id) {
-		try {
-			$('link#'+id).remove();
-		} catch(e) {}
-	}
-});
+        getStylesheet : function(url, id, media) {
+            var stylesheets = $("link[type='text/css']", $("head:first"));
+
+            var id = id || "rand" + (Math.floor(Math.random() * 10000));
+
+            var el = $('link#' + id, $('head:first'));
+            if (el.length > 0) {
+                el.attr('href', url);
+            } else {
+                $('<link />', {
+                    href : url,
+                    media : media || 'screen',
+                    type : 'text/css',
+                    rel : 'stylesheet',
+                    id : id
+                }).appendTo('head:first');
+            }
+        },
+        removeStylesheet : function(id) {
+            try {
+                $('link#' + id).remove();
+            } catch (e) {
+            }
+        }
+    });
 
 })(jQuery);
