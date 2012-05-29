feae840a3e1bc9aa8b07ca67d3bb16393b92c444
Alayn Gortazar <zutoin@gmail.com>
2012-05-29 12:48:18 +0000

    sin más
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@128 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,15 +2,13 @@
 
 class Klear_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-	/**
-	 * Registramos el plugin que se encarga de inicializar Klear
-	 * solo para los módulos de Klear
-	 */
-	protected function _initKlear()
-	{
-	    $front = Zend_Controller_Front::getInstance();
-	    
-	    $front->registerPlugin(new Klear_Plugin_Init());
+    /**
+     * Registramos los plugins necesarios para el correcto funcionamiento de Klear
+     */
+    protected function _initKlear()
+    {
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin(new Klear_Plugin_Init());
 
         /*
          * Klear_Plugin_Auth if enabled in klear.yaml
@@ -22,86 +20,86 @@ class Klear_Bootstrap extends Zend_Application_Module_Bootstrap
          */
         $front->registerPlugin(new Klear_Plugin_Translator());
 
-	}
+    }
 
-	protected function _initModuleRoutes()
-	{
-	    $frontController = Zend_Controller_Front::getInstance();
-	    $router = $frontController->getRouter();
+    protected function _initModuleRoutes()
+    {
+        $frontController = Zend_Controller_Front::getInstance();
+        $router = $frontController->getRouter();
 
-	    $router->addRoute(
-	        'klearDispatch',
-	        $route = new Zend_Controller_Router_Route(
-	            'klear/dispatch/:file/*',
-	            array(
-	                            'controller' => 'index',
-	                            'action' => 'dispatch',
-	                            'module' => 'klear'
-	                    )
-	        )
-	    );
-	}
+        $router->addRoute(
+            'klearDispatch',
+            new Zend_Controller_Router_Route(
+                'klear/dispatch/:file/*',
+                array(
+                    'controller' => 'index',
+                    'action' => 'dispatch',
+                    'module' => 'klear'
+                )
+            )
+        );
+    }
 
-	protected function _initAssetRoutes()
-	{
-		$frontController = Zend_Controller_Front::getInstance();
-		$router = $frontController->getRouter();
+    protected function _initAssetRoutes()
+    {
+        $frontController = Zend_Controller_Front::getInstance();
+        $router = $frontController->getRouter();
 
-		$router->addRoute(
-			'klearScripts',
-			$route = new Zend_Controller_Router_Route_Regex(
-    			'(default|klear[^/]*)/js/(.*)$',
-    			array(
-    				'controller' => 'assets',
-    				'action' => 'js',
-    				'module' => 'klear'
-    			),
-    			array(
-					2 => 'file',
-					1 => 'moduleName'
-    			)
-			)
-		);
+        $router->addRoute(
+            'klearScripts',
+            new Zend_Controller_Router_Route_Regex(
+                '(default|klear[^/]*)/js/(.*)$',
+                array(
+                    'controller' => 'assets',
+                    'action' => 'js',
+                    'module' => 'klear'
+                ),
+                array(
+                    2 => 'file',
+                    1 => 'moduleName'
+                )
+            )
+        );
 
-		$router->addRoute(
-			'klearCss',
-			$route = new Zend_Controller_Router_Route_Regex(
-				'(default|klear[^/]*)/css/(.*)$',
-				array(
-					'controller' => 'assets',
-					'action' => 'css',
-					'module' => 'klear'
-				),
-				array(
-					2 => 'file',
-					1 => 'moduleName'
-				)
-			)
-		);
-		
-		$router->addRoute(
-		        'klearCssExtended',
-		        $route = new Zend_Controller_Router_Route_Regex(
-		                '(default|klear[^/]*)/css-extended/(.*)/(.*)$',
-		                array(
-		                        'controller' => 'assets',
-		                        'action' => 'css-extended',
-		                        'module' => 'klear'
-		                ),
-		                array(
-		                        3 => 'file',
-		                        2 => 'plugin',
-		                        1 => 'moduleName'
-		                )
-		        )
-		);
+        $router->addRoute(
+            'klearCss',
+            new Zend_Controller_Router_Route_Regex(
+                '(default|klear[^/]*)/css/(.*)$',
+                array(
+                    'controller' => 'assets',
+                    'action' => 'css',
+                    'module' => 'klear'
+                ),
+                array(
+                    2 => 'file',
+                    1 => 'moduleName'
+                )
+            )
+        );
 
-		/*
-		 * TODO: Preparar la expresión regular para que soporte más tipos de imágenes
-		 */
-		$router->addRoute(
-	        'klearCssImages',
-	        $route = new Zend_Controller_Router_Route_Regex(
+        $router->addRoute(
+            'klearCssExtended',
+            new Zend_Controller_Router_Route_Regex(
+                '(default|klear[^/]*)/css-extended/(.*)/(.*)$',
+                array(
+                    'controller' => 'assets',
+                    'action' => 'css-extended',
+                    'module' => 'klear'
+                ),
+                array(
+                    3 => 'file',
+                    2 => 'plugin',
+                    1 => 'moduleName'
+                )
+            )
+        );
+
+        /*
+         * TODO: Preparar la expresión regular para que soporte más tipos de imágenes
+         */
+        $router->addRoute(
+            'klearCssImages',
+            new Zend_Controller_Router_Route_Regex(
                 '(default|klear[^/]*)/css/(.*\.png)$',
                 array(
                     'controller' => 'assets',
@@ -112,42 +110,39 @@ class Klear_Bootstrap extends Zend_Application_Module_Bootstrap
                     2 => 'file',
                     1 => 'moduleName'
                 )
-	        )
-		);
+            )
+        );
 
-		$router->addRoute(
-			'klearImages',
-			$route = new Zend_Controller_Router_Route_Regex(
-				'(default|klear[^/]*)/images/(.*)$',
-				array(
-					'controller' => 'assets',
-					'action' => 'image',
-					'module' => 'klear'
-				),
-				array(
-					2 => 'file',
-					1 => 'moduleName'
-				)
-			)
-		);
-
-		$router->addRoute(
-	        'klearBinaryAssets',
-	        $route = new Zend_Controller_Router_Route_Regex(
-                '(default|klear[^/]*)/bin/(.*)$',
+        $router->addRoute(
+            'klearImages',
+            new Zend_Controller_Router_Route_Regex(
+                '(default|klear[^/]*)/images/(.*)$',
                 array(
-                        'controller' => 'assets',
-                        'action' => 'bin',
-                        'module' => 'klear'
+                    'controller' => 'assets',
+                    'action' => 'image',
+                    'module' => 'klear'
                 ),
                 array(
-                        2 => 'file',
-                        1 => 'moduleName'
-	                )
-	        )
-		);
+                    2 => 'file',
+                    1 => 'moduleName'
+                )
+            )
+        );
 
-
-
-	}
+        $router->addRoute(
+            'klearBinaryAssets',
+            new Zend_Controller_Router_Route_Regex(
+                '(default|klear[^/]*)/bin/(.*)$',
+                array(
+                    'controller' => 'assets',
+                    'action' => 'bin',
+                    'module' => 'klear'
+                ),
+                array(
+                    2 => 'file',
+                    1 => 'moduleName'
+                )
+            )
+        );
+    }
 }
