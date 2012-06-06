d937fe0a4073fcad8ca0a8764f14524a8ee9a65c
David Lores <david@irontec.com>
2012-06-06 10:39:02 +0000

    Devolver this en metodos
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@140 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- models/SimpleResponse.php
+++ models/SimpleResponse.php
@@ -4,31 +4,36 @@
  * Clase respuesta Simple para peticiones desde klear.request.js
  * Respuestas simples de acciones concretas
  * No requieren carga de ficheros extra
- *  
+ *
  * @author jabi
  *
  */
-class Klear_Model_SimpleResponse {
-	
-	const RESPONSE_TYPE = 'simple'; 	
+class Klear_Model_SimpleResponse
+{
+
+	const RESPONSE_TYPE = 'simple';
 	protected $_data;
 
-	public function setData($data) {
+	public function setData($data)
+	{
 		$this->_data = $data;
+
+		return $this;
 	}
-	
-	public function attachView(Zend_View $view) {
+
+	public function attachView(Zend_View $view)
+	{
 		$view->data = $this->_data;
 		$view->responseType = self::RESPONSE_TYPE;
-		
+
 		$auth = Zend_Auth::getInstance();
-		
+
 		if (!$auth->hasIdentity()) {
-		    
+
 		    $view->mustLogIn = true;
 		}
+
+		return $this;
 	}
-	
-	
-	
+
 }
