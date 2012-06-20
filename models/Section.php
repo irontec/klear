a2850dab2a5a342f1fabb4641579f7f88912a504
Alayn Gortazar <zutoin@gmail.com>
2012-06-20 12:30:41 +0000

    Tabs y estilo
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@159 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- models/Section.php
+++ models/Section.php
@@ -5,94 +5,106 @@
 * @author jabi
 *
 */
-class Klear_Model_Section  implements Iterator {
+class Klear_Model_Section  implements Iterator
+{
 
-	protected $_name;
+    protected $_name;
 
-	protected $_description;
+    protected $_description;
 
-	protected $_menu = null;
+    protected $_menu = null;
 
-	protected $_subsections;
-	protected $_position = 0;
+    protected $_subsections;
+    protected $_position = 0;
 
-	protected $_skip = array();
+    protected $_skip = array();
 
-	public function setName($name) {
-		$this->_name = $name;
-		return $this;
-	}
+    public function setName($name)
+    {
+        $this->_name = $name;
+        return $this;
+    }
 
-	public function setParentMenu($menu) {
-	    $this->_menu = $menu;
-	    return $this;
-	}
-	/*
-	 * skip subsections
-	 */
-	public function setDataToSkip($skip)
-	{
-	    $this->_skip = $skip;
-	    return $this;
-	}
+    public function setParentMenu($menu)
+    {
+        $this->_menu = $menu;
+        return $this;
+    }
+    /*
+     * skip subsections
+     */
+    public function setDataToSkip($skip)
+    {
+        $this->_skip = $skip;
+        return $this;
+    }
 
-	public function setData(Zend_Config $data) {
+    public function setData(Zend_Config $data)
+    {
 
-		$config = new Klear_Model_KConfigParser();
-		$config->setConfig($data);
-		$this->_name = $config->getProperty("title",true);
-		$this->_description = $config->getProperty("description",false);
+        $config = new Klear_Model_KConfigParser();
+        $config->setConfig($data);
+        $this->_name = $config->getProperty("title", true);
+        $this->_description = $config->getProperty("description", false);
 
-		$this->_class = $config->getProperty("class",false);
+        $this->_class = $config->getProperty("class", false);
 
-		if (!isset($data->submenus)) return;
+        if (!isset($data->submenus)) return;
 
-		foreach($data->submenus as $file => $sectionData) {
-		    if (in_array($file, $this->_skip)) continue;
-		    $subsection = new Klear_Model_SubSection;
+        foreach ($data->submenus as $file => $sectionData) {
+            if (in_array($file, $this->_skip)) continue;
+            $subsection = new Klear_Model_SubSection;
 
-			$subsection
-			    ->setParentMenu($this->_menu)
-				->setMainFile($file)
-				->setData($sectionData);
+            $subsection
+                ->setParentMenu($this->_menu)
+                ->setMainFile($file)
+                ->setData($sectionData);
 
-			$this->_subsections[] = $subsection;
-		}
+            $this->_subsections[] = $subsection;
+        }
 
-	}
+    }
 
-	public function getName() {
-	    return $this->_name;
-	}
+    public function getName()
+    {
+        return $this->_name;
+    }
 
-    public function getDescription() {
-	    return $this->_description;
-	}
+    public function getDescription()
+    {
+        return $this->_description;
+    }
 
 
 
-	public function __construct() {
-		$this->_position = 0;
-	}
+    public function __construct()
+    {
+        $this->_position = 0;
+    }
 
-	public function rewind() {
-		$this->_position = 0;
-	}
+    public function rewind()
+    {
+        $this->_position = 0;
+    }
 
-	public function current() {
-		return $this->_subsections[$this->_position];
-	}
+    public function current()
+    {
+        return $this->_subsections[$this->_position];
+    }
 
-	public function key() {
-		return $this->_position;
-	}
+    public function key()
+    {
+        return $this->_position;
+    }
 
-	public function next() {
-		++$this->_position;
-	}
+    public function next()
+    {
+        ++$this->_position;
+    }
 
-	public function valid() {
-		return isset($this->_subsections[$this->_position]);
+    public function valid()
+    {
+        return isset($this->_subsections[$this->_position]);
 
-	}
+    }
 }
