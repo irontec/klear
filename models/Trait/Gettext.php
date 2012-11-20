87bc5d56e6f4bf7934fa44292beca9251186f946
Lander Ontoria <lander@irontec.com>
2012-11-20 15:38:53 +0000

    Soporte plurales
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@311 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- models/Trait/Gettext.php
+++ models/Trait/Gettext.php
@@ -8,18 +8,41 @@ trait Klear_Model_Trait_Gettext
      */
     protected function _gettextCheck($string)
     {
+        $pregMixed = preg_match_all(
+                    "|_\([\'\"](.*)[\'\"],[\s.]_\([\'\"](.*)[\'\"]\)|U",
+                    $string,
+                    $resultMixed,
+                    PREG_PATTERN_ORDER);
         $preg = preg_match_all(
                 "|_\([\'\"](.*)[\'\"]\)|U",
                 $string,
                 $result,
                 PREG_PATTERN_ORDER);
-        if ($preg && is_array($result)) {
+        
+        $pregPlural = preg_match_all(
+                "|ngettext\([\'\"](.*)[\'\"],[\s][\'\"](.*)[\'\"],[\s](.*)\)|U",
+                $string,
+                $resultPlural,
+                PREG_PATTERN_ORDER);
+        
+        $translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
+        if ($pregMixed == 1 && count($resultMixed) == 3) {
             $replace = array();
-            $translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
+            foreach ($resultMixed[2] as $match) {
+                $item = $translator->{'translate'}($match);
+            }
+            foreach ($resultMixed[1] as $match) {
+                $string = sprintf($translator->{'translate'}($match), $item);
+            }
+        } elseif ($preg == 1 && is_array($result)) {
+            $replace = array();
             foreach ($result[1] as $match) {
                 $replace[] = $translator->{'translate'}($match);
             }
             $string = str_replace($result[0], $replace, $string);
+        } elseif ($pregPlural == 1 && count($resultPlural) == 4 ) {
+            $item = $translator->{'plural'}($resultPlural[1][0], $resultPlural[2][0], $resultPlural[3][0]);
+            $string = str_replace($resultPlural[0][0], $item, $string);
         }
         return $string;
     }
