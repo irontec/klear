e2deb7778154fcfdc541dae9e95b87458ba79429
Javier Infante <jabi+github@irontec.com>
2013-02-08 09:17:43 +0000

    Soporte para gettext en yaml mejorado:   métodos soprotados: 	"_" y "ngettext"
    "_" >> Ahora sopora sintaxis sprintf
    	Los parámetros pueden ser invocaciones a "_" y a "ngettext", literales quoteados y/o números.
    "ngettext" > Soporta múltiples plurales
    
    No se puede concatenar sin mas (hay que hacer utilizando sintaxis sprintf)
    
    _("Listado de %s",ngettext("entidad","entidades",2))
    _("Bienvenido a la empresa %2$s, usuario %1$s","${auth.login}","${auth.companyName}")
    
    
    
    
    
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@334 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- models/Gettext.php
+++ models/Gettext.php
@@ -1,6 +1,8 @@
 <?php
 class Klear_Model_Gettext
 {
+
+    
     /**
      * Miramos "a lo gettext para traducirlo automáticamente"
      *
@@ -8,46 +10,131 @@ class Klear_Model_Gettext
      */
     public static function gettextCheck($string)
     {
-        $pregMixed = preg_match_all(
-            "|_\([\'\"](.*)[\'\"],[\s.]_\([\'\"](.*)[\'\"]\)|U",
-            $string,
-            $resultMixed,
-            PREG_PATTERN_ORDER
-        );
-
-        $preg = preg_match_all(
-            "|_\([\'\"](.*)[\'\"]\)|U",
-            $string,
-            $result,
-            PREG_PATTERN_ORDER
-        );
-
-        $pregPlural = preg_match_all(
-            "|ngettext\([\'\"](.*)[\'\"],[\s][\'\"](.*)[\'\"],[\s](.*)\)|U",
-            $string,
-            $resultPlural,
-            PREG_PATTERN_ORDER
-        );
-
-        $translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
-        if ($pregMixed == 1 && count($resultMixed) == 3) {
-            $replace = array();
-            foreach ($resultMixed[2] as $match) {
-                $item = $translator->{'translate'}($match);
-            }
-            foreach ($resultMixed[1] as $match) {
-                $string = sprintf($translator->{'translate'}($match), $item);
-            }
-        } elseif ($preg == 1 && is_array($result)) {
-            $replace = array();
-            foreach ($result[1] as $match) {
-                $replace[] = $translator->{'translate'}($match);
-            }
-            $string = str_replace($result[0], $replace, $string);
-        } elseif ($pregPlural == 1 && count($resultPlural) == 4 ) {
-            $item = $translator->{'plural'}($resultPlural[1][0], $resultPlural[2][0], $resultPlural[3][0]);
-            $string = str_replace($resultPlural[0][0], $item, $string);
+        
+        $validFunctions = array("ngettext","_");
+        
+        $string = trim($string);
+        
+        //Detectamos que el literal no esté contenido en una de las funciones gettext
+        
+        $quotedValidFunctions = array();
+        foreach($validFunctions as $funcName) {
+            $quotedValidFunctions[] = '' . preg_quote($funcName. '(' ,"/") . '';
         }
-        return $string;
+
+        if (!preg_match("/^".implode("|", $quotedValidFunctions)."/i", $string, $result)) {
+
+            return $string;
+        }
+        
+        $tokens = token_get_all("<?php ".$string." ?>");
+        
+        $literal = '';
+        $opened = 0;
+        $curFunction = false;
+        $arguments = array();
+        
+        $totalTokens = sizeof($tokens);
+        
+        foreach($tokens as $idToken => $token) {
+            if (is_string($token)) {
+                switch($token) {
+                    case ')':
+                        $opened -=1;
+                        if ($opened == 0) {
+                            break 2;
+                        }
+                        
+                        break;
+                        
+                    case '(':
+                        $opened +=1;
+                        break;
+                    
+                }
+                continue;
+            }
+            
+            switch(token_name($token[0])) {
+                case 'T_STRING':
+                    
+                    // Nombre de función 
+                    if  (in_array($token[1], $validFunctions)) {
+                        
+                        if ($curFunction === false) {
+                            $curFunction = $token[1];
+                            $arguments = array();
+                        } else {
+                            
+                            $newString = '';
+                            
+                            for($j = $idToken;$j<$totalTokens;$j++) {
+                                if (is_string($tokens[$j])) {
+                                    
+                                    $newString .= $tokens[$j];
+                                } else {
+                                    $newString .= $tokens[$j][1];
+                                }
+                            }
+                            
+                            $arguments[] = self::gettextCheck($newString);
+                            
+                        }
+                            
+                    } else {
+                        var_dump($string, $token);echo "WTF";exit;
+
+                        Throw new Exception("Invalid gettext string");
+
+                    }       
+                    
+                    break;
+                case 'T_CONSTANT_ENCAPSED_STRING':
+                case 'T_LNUMBER':
+                case 'T_DNUMBER':
+                    // Literal string (con comillas o espacios en blanco)
+                    // Literal número
+                    if (($curFunction === false) || ($opened == 0)) {
+                        Throw new Exception("Invalid gettext string");
+                    }
+                    if ($opened == 0) {
+                        $literal .= $token[1];
+                    }
+                    if ($opened == 1) {
+                        $arguments[] = preg_replace("|^[\'\"](.*)[\'\"]$|", "$1", $token[1]);
+                    }                              
+                
+                    break;
+                case 'T_WHITESPACE':
+                    $literal .= $token[1];
+                    break;
+                
+            }
+            
+        }
+        
+        $translator = Zend_Registry::get(Klear_Plugin_Translator::DEFAULT_REGISTRY_KEY);
+        
+        switch($curFunction) {
+            case "_":
+                
+                $arguments[0] = $translator->translate($arguments[0]);
+                
+                if (sizeof($arguments) > 1) {
+                    return call_user_func_array("sprintf" , $arguments);
+                } else {
+                    return $arguments[0];
+                }
+                break;
+                
+            case "ngettext":
+
+                return call_user_func_array(array($translator, 'plural'), $arguments);
+                break;
+        }
+
+        Throw Exception("Invalid function in gettext");
+
+        
     }
 }
