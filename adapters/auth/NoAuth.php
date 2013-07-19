16926189069b5cc6314c01f649ba7a6556fcbfda
Alayn Gortazar <zutoin@gmail.com>
2013-07-19 09:10:33 +0000

    #0032981: Permitir usar klear sin usuario y contraseña
    Creado adaptador Klear_Auth_Adapter_NoAuth, pide usuario y contraseña pero da igual lo que le digamos porque no hace nada, solo confirma la autenticación
    
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@404 32e02440-ed3a-45b5-ab12-845b16f45b8b

---
+++ adapters/auth/NoAuth.php
@@ -0,0 +1,39 @@
+<?php
+
+class Klear_Auth_Adapter_NoAuth implements \Klear_Auth_Adapter_KlearAuthInterface
+{
+    protected $_username;
+    protected $_password;
+    protected $_userId;
+
+    protected $_userMapper;
+
+
+    /**
+     *
+     * @var Klear_Auth_Adapter_Interfaces_BasicUserModel
+     */
+    protected $_user;
+
+    public function __construct(Zend_Controller_Request_Abstract $request, Klear_Model_ConfigParser $authConfig = null)
+    {
+        $this->_username = $request->getPost('username', '');
+    }
+
+    public function authenticate()
+    {
+        $this->_user = new \Klear_Model_User();
+        $this->_user->setLogin($this->_username);
+
+        $authResult = Zend_Auth_Result::SUCCESS;
+        $authMessage = array("message"=>"Welcome!");
+        return new Zend_Auth_Result($authResult, $this->_username, $authMessage);
+    }
+
+    public function saveStorage()
+    {
+        $auth = Zend_Auth::getInstance();
+        $authStorage = $auth->getStorage();
+        $authStorage->write($this->_user);
+    }
+}
