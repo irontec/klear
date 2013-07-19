947f029e6ba35bf8bf7f12a248073900a0f1a487
Javier Infante <jabi+github@irontec.com>
2013-07-19 11:54:34 +0000

    Fixes for Jenkinks #32987
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@406 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- views/helpers/Toolsbar.php
+++ views/helpers/Toolsbar.php
@@ -10,10 +10,10 @@ class Klear_View_Helper_Toolsbar extends Klear_View_Helper_Base
     public function Toolsbar()
     {
     	$ret = '<input type="checkbox" id="superCollapse" /> ' .
-    			'<label for="superCollapse" title="'. $this->_view->translate("Collapse All.").'">' .
-    			'<span class="ui-icon ui-icon-triangle-1-nw" ></span>' .
-    			'</label>';
-    	
+    	       	'<label for="superCollapse" title="'. $this->_view->translate("Collapse All.").'">' .
+    	       	'<span class="ui-icon ui-icon-triangle-1-nw" ></span>' .
+    	       	'</label>';
+
 //     	$ret .= '<input type="checkbox" id="menuCollapse" /> ' .
 //             '<label for="menuCollapse" title="'. $this->_view->translate("Collapse Menu.").'">' .
 //             '<span class="ui-icon ui-icon-triangle-1-w" ></span>' .
@@ -23,37 +23,38 @@ class Klear_View_Helper_Toolsbar extends Klear_View_Helper_Base
 //         		'<label for="headerCollapse" title="'. $this->_view->translate("Collapse Header.").'">' .
 //         		'<span class="ui-icon ui-icon-triangle-1-n" ></span>' .
 //         		'</label>';
-        
+
         $ret .= '<input type="checkbox" id="tabsPersist" />' .
             '<label for="tabsPersist" title="' .$this->_view->translate("Remember opened main tabs.") . '">'.
             '<span class="ui-icon ui-icon-unlocked" ></span>' .
             '</label>';
 
         if ($langs = $this->_view->SiteConfig()->getLangs()) {
-        	if (sizeof($langs) > 1) {
-        		$ret .= '<input type="checkbox" id="langPicker" '.
-        		'/><label for="langPicker" title="' . $this->_view->translate("Cambiar de Idioma") .'">' .
-        		'<span class="ui-icon ui-icon-flag" ></span>' .
-        		'</label>';
-        		
-        		foreach ($langs as $_langIden => $lang) {
-        			$ret .= '<input type="radio" name="lang" class="pickableLang" value="'.$_langIden.'"  id="lang' .$_langIden.'" '.
-          			(($_langIden == $this->_view->SiteConfig()->getLang()->getIden())? ' checked="checked"':'') . 
-        			'/><label for="lang'.$_langIden.'" title="' . $lang.'" class="expanded pickableLanguage">' .
-        			$lang .         			
-        			'</label>';
-        		}
-        	}
+            if (sizeof($langs) > 1) {
+                $ret .= '<input type="checkbox" id="langPicker" '.
+                        '/><label for="langPicker" title="' . $this->_view->translate("Cambiar de Idioma") .'">' .
+                        '<span class="ui-icon ui-icon-flag" ></span>' .
+                        '</label>';
+
+                foreach ($langs as $_langIden => $lang) {
+                    $ret .= '<input type="radio" name="lang" class="pickableLang" value="'.$_langIden.'" ' .
+                            ' id="lang' .$_langIden.'" '.
+                            (($_langIden == $this->_view->SiteConfig()->getLang()->getIden())? ' checked="checked"':'') . 
+                            '/><label for="lang'.$_langIden.'" title="' . $lang.'" class="expanded pickableLanguage">' .
+                            $lang .
+                            '</label>';
+                }
+            }
         }
         
         
         $ret .= '<input type="checkbox" id="logout" '.
-        		'data-url="' . $this->_view->url(array('controller' => 'index', 'action' => 'bye')).'"' .
-        		'/><label for="logout" title="' . $this->_view->translate("logout") .'">' .
-        		'<span class="ui-icon ui-icon-power" ></span>' .
-        		'</label>'; 
-        
+                'data-url="' . $this->_view->url(array('controller' => 'index', 'action' => 'bye')).'"' .
+                '/><label for="logout" title="' . $this->_view->translate("logout") .'">' .
+                '<span class="ui-icon ui-icon-power" ></span>' .
+                '</label>'; 
+
         return $ret;
-            		
+
     }
 }
