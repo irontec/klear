8fd221a24fde5d997e57c4e7a862866622e3ddf1
Javier Infante <jabi+github@irontec.com>
2013-07-23 09:31:27 +0000

    Se modifica el yaml de themes para que sean escogibles los de nuestro propio cdn. Se adecua el parseardor para soportar varios cdns. #33029
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@412 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- models/JQueryUIThemeParser.php
+++ models/JQueryUIThemeParser.php
@@ -6,7 +6,6 @@ class Klear_Model_JQueryUIThemeParser
     const filename = 'jquery-ui-themes.yaml';
 
     protected $_config;
-    protected $_baseUrl;
 
     protected $_localConfig = false;
     protected $_localBaseUrl = false;
@@ -82,17 +81,39 @@ class Klear_Model_JQueryUIThemeParser
             $cache->save($this->_config);
         }
 
-        $this->_baseUrl = $this->_config->baseurl;
     }
 
 
+    protected function _parseForTheme(Zend_Config $config, $targetTheme)
+    {
+        foreach ($config->themes as $_theme) {
+            if ($targetTheme === trim($_theme)) {
+                $themePath = str_replace('%theme%', $_theme, $config->baseurl);
+                return $themePath;
+            }
+        }
+
+        return false;
+    }
+
     public function getPathForTheme($theme)
     {
-        foreach ($this->_config->themes as $_theme) {
 
-            if ($theme === trim($_theme)) {
 
-                return str_replace('%theme%', $_theme, $this->_baseUrl);
+        if (isset($this->_config->multicdn)) {
+            foreach ($this->_config->multicdn as $cdnConfig) {
+                $foundPath = $this->_parseForTheme($cdnConfig, $theme);
+                if ($foundPath !== false) {
+                    return $foundPath;
+                }
+            }
+        }
+
+        // old school config jquery-ui themes config file (no multicdn)
+        if (isset($this->_config->themes)) {
+            $foundPath = $this->_parseForTheme($config, $theme);
+            if ($foundPath !== false) {
+                return $foundPath;
             }
         }
 
@@ -101,7 +122,8 @@ class Klear_Model_JQueryUIThemeParser
             foreach ($this->_localConfig->themes as $_theme) {
 
                 if ($theme === trim($_theme)) {
-                    return str_replace('%theme%', $_theme, $this->_localBaseUrl);
+                    return
+                        str_replace('%theme%', $_theme, $this->_localBaseUrl);
                 }
             }
         }
