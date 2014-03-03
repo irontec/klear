32e43a4f075cdf30c6d96d66596f35f2bec86551
David Lores <david@irontec.com>
2014-03-03 16:43:36 +0000

    #0037830 Bug solucionado en seleccion de idioma en login, por variable no inicializada
    git-svn-id: http://dev.irontec.com/svn/klear/trunk/modules/klear@462 32e02440-ed3a-45b5-ab12-845b16f45b8b

--- views/helpers/LanguageSelector.php
+++ views/helpers/LanguageSelector.php
@@ -9,34 +9,38 @@ class Klear_View_Helper_LanguageSelector extends Klear_View_Helper_Base
 
     public function LanguageSelector()
     {
+        $ret = '';
+
         if ($langs = $this->_view->SiteConfig()->getLangs()) {
-            if (sizeof($langs) > 1) {
-                $ret = '<div id="loginTools">
+
+            if (count($langs) > 1) {
+
+                $ret .= '<div id="loginTools">
                 <div id="loginToolsbar">';
-                
-                $ret.= '<input type="checkbox" id="langPickerLogin" '.
-                        '/><label for="langPickerLogin" title="' . $this->_view->translate("Change language") .'" class="ui-corner-right">' .
+
+                $ret .= '<input type="checkbox" id="langPickerLogin" '.
+                        '/><label for="langPickerLogin" title="' . $this->_view->translate("Change language") . '" class="ui-corner-right">' .
                         '<span class="ui-icon ui-icon-flag " ></span>' .
                         '</label>';
 
                 foreach ($langs as $_langIden => $lang) {
-                    $ret .= '<input type="radio" name="lang" class="pickableLang" value="'.$_langIden.'" ' .
-                            ' id="lang' .$_langIden.'Login" ';
+
+                    $ret .= '<input type="radio" name="lang" class="pickableLang" value="' . $_langIden . '" ' .
+                            ' id="lang' . $_langIden . 'Login" ';
+
                     if ($_langIden == $this->_view->SiteConfig()->getLang()->getIden()) {
-                        $ret .= ' checked="checked"'; 
+                        $ret .= ' checked="checked"';
                     }
 
-                    $ret .= '/><label for="lang'.$_langIden.'Login" title="' . $lang.'" class="expanded pickableLanguage">' .
+                    $ret .= '/><label for="lang' . $_langIden . 'Login" title="' . $lang . '" class="expanded pickableLanguage">' .
                             $lang .
                             '</label>';
                 }
 
-                $ret.= '</div></div>';
+                $ret .= '</div></div>';
             }
         }
-        
 
         return $ret;
-
     }
 }
